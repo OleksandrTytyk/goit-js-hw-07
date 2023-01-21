3d54a048d723f981646b152c647ba499aaa956ac
Oleksandr Tytyk <aleks.tit3@gmail.com>
2023-01-22 00:16:46 +0200

    Update 01-gallery.js

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -20,16 +20,28 @@ const marcupList = galleryItems
 
 imgGalleryRef.innerHTML = marcupList;
 
-imgGalleryRef.addEventListener("click", onGalleryModalClick);
-function onGalleryModalClick(evt) {
-  evt.preventDefault();
+imgGalleryRef.addEventListener("click", onGalleryModalOpen);
 
+function onGalleryModalOpen(evt) {
+  evt.preventDefault();
   if (evt.target.nodeName !== "IMG") {
     return;
   }
 
-  const instance = basicLightbox.create(`
+  const instance = basicLightbox.create(
+    `
     <img src="${evt.target.dataset.source}" width="800" height="600">
-`);
+`,
+    {
+      onShow: (instance) => window.addEventListener("keydown", onGalleryModalClose),
+      onClose: (instance) => window.removeEventListener("keydown", onGalleryModalClose),
+    }
+  );
+  function onGalleryModalClose(evt) {
+    if (evt.code === 'Escape') {
+     instance.close();
+  }
+}
+
   instance.show();
 }
