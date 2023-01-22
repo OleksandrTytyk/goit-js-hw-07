2807eed94ab1c59a31c2a3d8add57d2454996d7d
Oleksandr Tytyk <aleks.tit3@gmail.com>
2023-01-22 13:33:23 +0200

    add 2 tasc

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,16 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+const imgGalleryRef = document.querySelector(".gallery");
+
+const marcupList = galleryItems.map(img => {
+    return `<a class="gallery__item" href="${img.original}">
+  <img class="gallery__image" src="${img.preview}" alt="${img.description}" />
+</a>`;
+}).join('')
+
+imgGalleryRef.innerHTML = marcupList;
+
+const lightbox = new SimpleLightbox(".gallery a", {
+  captionsData: 'alt',
+  captionDelay: '250'
+});
